7b550c34c174cc723d93c631609c12b8b61cda38
ccmansilla <carloscmansilla@gmail.com>
2022-10-08 09:10:23 -0300

    mejoro el css

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ async function load() {
     let primero = 'active';
     mascotas.forEach(mascota => {
         let img = mascota.img;
-        let texto = mascota.nombre;
+        let texto = `${mascota.nombre} tiene ${mascota.edad} de edad`;
         let plantilla = `<div class="carousel-item ${primero}">
                             <div class='img'>
                                 <img src="${img}" alt="...">
