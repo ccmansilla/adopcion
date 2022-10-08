56c0cafa0631ab23cd644cb66d8619cf03ad7138
ccmansilla <carloscmansilla@gmail.com>
2022-10-08 08:59:21 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+let contenedor = document.querySelector('.carousel-inner');
+async function load() {
+    const response = await fetch('http://127.0.0.1:5500/js/mascotas.json');
+    const mascotas = await response.json();
+
+    let primero = 'active';
+    mascotas.forEach(mascota => {
+        let img = mascota.img;
+        let texto = mascota.nombre;
+        let plantilla = `<div class="carousel-item ${primero}">
+                            <div class='img'>
+                                <img src="${img}" alt="...">
+                            </div>
+                            <div class="texto">${texto}</div>
+                        </div>`;
+        contenedor.innerHTML += plantilla;
+        primero = '';
+    });
+}
+load();
+
