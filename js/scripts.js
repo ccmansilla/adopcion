c1924dbacbf0d7897a2d407970ff1262c7bb7663
ccmansilla <carloscmansilla@gmail.com>
2022-10-09 19:43:48 -0300

    clase animal agregada

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+class Animal{
+    constructor(objeto){
+        this.tipo = objeto.tipo;
+        this.nombre = objeto.nombre;
+        this.edad = objeto.edad;
+        this.img = objeto.img;
+    }
+
+    info(){
+        return `Soy un ${this.tipo} mi nombre es ${this.nombre} y tengo ${this.edad}`;
+    }
+}
+
 let contenedor = document.querySelector('.carousel-inner');
 async function load() {
     const response = await fetch('./js/mascotas.json');
     const mascotas = await response.json();
 
     let primero = 'active';
-    mascotas.forEach(mascota => {
+    mascotas.forEach(objeto => {
+        mascota = new Animal(objeto);
         let img = mascota.img;
-        let texto = `${mascota.nombre} tiene ${mascota.edad} de edad`;
+        let texto = mascota.info();
         let plantilla = `<div class="carousel-item ${primero}">
                             <div class='img'>
                                 <img src="${img}" alt="...">
